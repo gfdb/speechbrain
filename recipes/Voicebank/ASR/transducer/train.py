7159892e0d7b91693975a6f0bb5d005c2c6b2fcd
gfdb <francodumoulin123@gmail.com>
2025-05-25 18:42:03 -0400

    fixed spd perturb prob

--- recipes/Voicebank/ASR/transducer/train.py
+++ recipes/Voicebank/ASR/transducer/train.py
@@ -245,7 +245,7 @@ def dataio_prep(hparams):
     def audio_pipeline_train(wav):
         sig = sb.dataio.dataio.read_audio(wav)
         if "usa_speed" in hparams and hparams["usa_speed"] and "speed_perturb" in hparams:
-            spd_prob = 0.5 # probability to apply speed perturbation 
+            spd_prob = 0.1574 # probability to apply speed perturbation 
             if torch.randn((1,)).item() > spd_prob:
                 sig = hparams["speed_perturb"](sig.unsqueeze(0))
                 sig = sig.squeeze(0)
