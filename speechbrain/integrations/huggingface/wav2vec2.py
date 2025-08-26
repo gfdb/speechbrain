d1b54c44025c8c36fca33e64a484ce41b18cd7a5
gfdb <francodumoulin123@gmail.com>
2025-08-25 21:47:24 -0400

    fix feat_mask bug

--- speechbrain/integrations/huggingface/wav2vec2.py
+++ speechbrain/integrations/huggingface/wav2vec2.py
@@ -91,8 +91,10 @@ class Wav2Vec2(HFTransformersInterface):
         )
 
         self.model.config.apply_spec_augment = apply_spec_augment
+        # pull custom knobs so they don't reach the parent
+        feat_mask = kwargs.pop("feat_mask", False)
 
-        if apply_spec_augment and 'feat_mask' in kwargs and kwargs['feat_mask'] is True:
+        if apply_spec_augment and feat_mask:
             self.model.config.mask_feature_prob = 0.008
             self.model.config.mask_feature_length = 64
 
