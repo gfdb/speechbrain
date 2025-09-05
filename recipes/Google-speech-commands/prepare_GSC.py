83052a4c837d26c7fa6943f1c8262080fe5d8052
gfdb <francodumoulin123@gmail.com>
2025-09-04 22:13:42 -0700

    fix gsc existence check

--- recipes/Google-speech-commands/prepare_GSC.py
+++ recipes/Google-speech-commands/prepare_GSC.py
@@ -134,7 +134,7 @@ def prepare_GSC(
         return
 
     # If the data folders do not exist, we need to extract the data
-    if not os.path.isdir(os.path.join(data_folder, "train-synth")):
+    if not all(os.path.isdir(os.path.join(data_folder, word)) for word in all_words):
         # Check for zip file and download if it doesn't exist
         tar_location = os.path.join(data_folder, "speech_commands_v0.02.tar.gz")
         if not os.path.exists(tar_location):
